4264c056c512098bc543d295b2c955066976647c
WilliamEStory <wstory3@gatech.edu>
2018-04-17 22:03:54 -0400

    Use multiple return values

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	hand, remainingDeck := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("Remaining")
+	remainingDeck.print()
 }
 
 func newCard() string {
