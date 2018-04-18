ac36b2010361ec87946487a45a4e08746b32e4a2
WilliamEStory <wstory3@gatech.edu>
2018-04-17 21:21:14 -0400

    Learn about func signatures

--- cards/main.go
+++ cards/main.go
@@ -5,9 +5,11 @@ import (
 )
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of spades"
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
