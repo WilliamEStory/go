4f4992b0ebf42c6515570ab9ed37720444489e85
WilliamEStory <william.story.e@gmail.com>
2018-04-19 17:36:16 -0400

    Write print, updateName method for person struct

--- structs/main.go
+++ structs/main.go
@@ -12,18 +12,26 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
+	jim.updateName("jimmy")
+	jim.print()
+}
 
-	fmt.Printf("%+v", jim)
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
