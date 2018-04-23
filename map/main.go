a861b77a5b73a1fafbf6e9d1d9660fb92a3d8120
WilliamEStory <william.story.e@gmail.com>
2018-04-23 18:11:45 -0400

    Learn about maps

---
+++ map/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4b745",
+	// }
+
+	fmt.Println(colors)
+}
